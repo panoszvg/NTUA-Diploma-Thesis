c2f7bbcb09f14303ca6a6109118847f42fda9f4d
Panagiotis Zevgolatakos <panoszevg@outlook.com>
2022-05-09 15:21:31 +0300

    Add QUESTION subscriber

--- answers/index.js
+++ answers/index.js
@@ -1,4 +1,30 @@
 const app = require('./app');
+const subscribe = () => {
+    const { Kafka } = require("kafkajs")
+
+    const kafka = new Kafka({
+        clientId: "consumer",
+        brokers: ['panos-Inspiron-7570:9092'],
+    })
+
+    const consumer = kafka.consumer({ groupId: "my-consumer-group" })
+
+    const run = async() => {
+        await consumer.connect()
+        await consumer.subscribe({ topic: "QUESTION", fromBeginning: true })
+        await consumer.run({
+            eachMessage: async({ topic, partition, message }) => {
+                const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+                const payload = `- ${prefix} ${message.key}#${message.value}`
+                console.log(payload)
+            }
+        })
+    }
+
+    run().catch(e => console.error(`[kafka-consumer] ${e.message}`, e))
+}
+
+subscribe();
 
 const port = Number(4002);
 
