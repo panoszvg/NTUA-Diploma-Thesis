c6e83fd2ceef961ed009d049b175d97443770bd2
panoszvg <44406295+panoszvg@users.noreply.github.com>
2022-05-18 13:49:11 +0300

    Make Question Page functional

--- frontend/src/QuestionPage.js
+++ frontend/src/QuestionPage.js
@@ -1,62 +1,98 @@
 import React, { Component } from "react";
+import { useParams } from "react-router";
 import './QuestionPage.css';
 const axios = require('axios');
 
 class Keyword extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: props.keyword,
+        }
+    }
+
     render() {
         return(
-            <p className="Keyword">Keyword</p>
+            <p className="Keyword">{this.state.keyword}</p>
         )
     }
 }
 
 class Answer extends Component {
 
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answer: props.answer,
+        }
+    }
+
+    render() {
         return(
-            <li className="Answer">This is an answer</li>
+            <li className="Answer">{this.state.answer}</li>
         )
     }
 }
 
+const QuestionPageWrapper = () => {
+    const { id } = useParams();
+    return <QuestionPage id={id}/>;
+}
+
 class QuestionPage extends Component {
 
-    constructor() {
+    constructor(props) {
         super();
+        this.state = {
+            id: props.id,
+            title: '',
+            text: '',
+            dateCreated: '',
+            keywords: [],
+            answers: []
+        }
     }
 
     componentDidMount() {
         const config = {
             method: 'GET',
-            url: "https://ntua-thesis-answers.herokuapp.com/question/",
+            url: `https://ntua-thesis-answers.herokuapp.com/question/${this.state.id}`,
             headers: {'Content-Type': 'application/json'}
         }
         axios(config)
         .then(res => {
-            this.setState({questions: res.data.questions});
+            console.log(res.data)
+            this.setState({title: res.data.question.title});
+            this.setState({text: res.data.question.text});
+            this.setState({dateCreated: res.data.question.dateCreated});
+            this.setState({keywords: res.data.question.keywords[0]});
+            this.setState({answers: (res.data.answers.length != 0) ? res.data.question.answers : []});
         })
         .catch(err => {
             console.log(err);
         })
     }
 
+    _renderKeywords(keyword, index) {
+        return (<Keyword key={index} keyword={keyword}/>)
+    }
+
+    _renderAnswers(answer, index) {
+        return (<Answer key={index} answer={answer}/>)
+    }
+
     render() {
+        console.log(this.state.keywords)
         return(
             <div className="QuestionPage">
-                <h1>This is a question</h1>
-                <h3>This is the text of the question. What seems to be the best way to make it appear good?</h3>
-                <div className="row">
-                    <Keyword/>
-                    <Keyword/>
-                </div>
+                <h1>{this.state.title}</h1>
+                <h3>{this.state.text}</h3>
+                <div className="row">{this.state.keywords.map(this._renderKeywords)}</div>
                 <br /><hr />
                 <h2>Answers</h2>
                 <br />
-                <div className="AnswersContainer">
-                    <Answer/>
-                    <Answer/>
-                </div>
+                <div className="AnswersContainer">{(this.state.answers != []) ? this.state.answers.map(this._renderAnswers) : <br />}</div>
                     <br /><br />
                     <textarea name="" id="" cols="70" rows="10"></textarea>
                     <br /><br /><br />
@@ -67,4 +103,4 @@ class QuestionPage extends Component {
 
 }
 
-export default QuestionPage;
+export default QuestionPageWrapper;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Navbar from './Navbar';
 import QuestionForm from './QuestionForm';
 import QuestionCardList from './QuestionCard';
-import QuestionPage from './QuestionPage';
+import QuestionPageWrapper from './QuestionPage';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -15,7 +15,7 @@ root.render(
         <Routes>
             <Route path="/" element={<QuestionForm/>} />
             <Route path="/browse" element={<QuestionCardList/>} />
-            <Route path="/question/:id" element={<QuestionPage/>} />
+            <Route path="/question/:id" element={<QuestionPageWrapper/>} />
         </Routes>
     </BrowserRouter>
 );
