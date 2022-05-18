bc979ff34ad8065e587da8804c7e49d57a46a6a6
panoszvg <44406295+panoszvg@users.noreply.github.com>
2022-05-18 11:51:30 +0300

    Add CORS to Ask Questions

--- ask-questions/app.js
+++ ask-questions/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const createQuestion = require('./createQuestion')
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', createQuestion);
 app.use((req, res, next) => { res.status(404).json({ message: 'Endpoint not found!' }); })
