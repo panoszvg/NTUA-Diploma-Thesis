4724668d28e1fee812ee37e0dd2d1f15ace27577
panoszvg <44406295+panoszvg@users.noreply.github.com>
2022-06-05 22:46:21 +0300

    Create graphs.js

---
+++ blockchain-frontend/pages/graphs.js
@@ -0,0 +1,17 @@
+import { Bar, Line } from "react-chartjs-2";
+
+export function barChart(data) {
+    return(
+        <div>
+            <Bar></Bar>
+        </div>
+    );
+}
+
+export function lineChart(data) {
+    return(
+        <div>
+            <Line></Line>
+        </div>
+    );
+}
