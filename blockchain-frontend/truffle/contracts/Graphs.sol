befe01f2aac63b43f7d6627e071240a33ace3a6c
panoszvg <44406295+panoszvg@users.noreply.github.com>
2022-06-09 10:59:25 +0300

    Update Graphs.sol
    Add noKeywords variable and requirements

--- blockchain-frontend/truffle/contracts/Graphs.sol
+++ blockchain-frontend/truffle/contracts/Graphs.sol
@@ -10,28 +10,40 @@ contract Graphs {
         string[] keywords;
         uint questions;
         uint answers;
+        uint noKeywords; // number of questions that don't have keywords
     }
 
     // map a uint that represents date to a custom struct
     mapping(uint => Day) daysStorage;
 
-    // add keywords, question, answers
-    function addToDay(uint date, string memory newKeywordsString, uint newQuestions, uint newAnswers) public {
-        require(date % 86400 == 0 , "Date given must have a time of 00:00:00");
-        string[] memory newKeywords = newKeywordsString.split(",");
-        // add new keywords to day
-        for (uint i = 0; i < newKeywords.length; i++) {
-            daysStorage[date].keywords.push(newKeywords[i]);
+    // add keywords, question, answers, noKeywords
+    function addToDay(uint date, string memory newKeywordsString, uint newQuestions, uint newAnswers, uint newNoKeywords) public {
+        require(date % 86400 == 0 , "Date given must have a time of 00:00:00.");
+        // keywords are given
+        if (bytes(newKeywordsString).length != 0) {
+            require(newQuestions >= (newNoKeywords + 1), "The number of questions doesn't match the sum of questions with and without keywords.");
+            string[] memory newKeywords = newKeywordsString.split(",");
+            // add new keywords to day
+            for (uint i = 0; i < newKeywords.length; i++) {
+                daysStorage[date].keywords.push(newKeywords[i]);
+            }
         }
-        // update questions/answers number
+        else {
+            require(newQuestions >= newNoKeywords, "The number of questions doesn't match the sum of questions with and without keywords.");
+        }
+        // update questions/answers/noKeywords number
         daysStorage[date].questions += newQuestions;
         daysStorage[date].answers += newAnswers;
+        daysStorage[date].noKeywords += newNoKeywords;
     }
 
     // get (keywords, questionsPerDay, answersPerDay) for a period of time (dateFrom, DateTo)
     function getDays(uint dateFrom, uint dateTo) public view
-    returns (string[] memory keywords, uint[] memory questions, uint[] memory answers) {
-        require((dateTo - dateFrom) % 86400 == 0 , "Dates given must have a time of 00:00:00");
+    returns (string[] memory keywords, uint[] memory questions, uint[] memory answers, uint[] memory noKeywords) {
+        require(dateFrom % 86400 == 0 , "First date given must have a time of 00:00:00");
+        require(dateTo % 86400 == 0 , "Second date given must have a time of 00:00:00");
+        require(dateFrom <= dateTo , "Second date given must be the same or later than the first date");
+        
         uint totalDays = ((dateTo - dateFrom) / 86400) + 1;
         
         // get number of all keywords that will be returned to create fixed size array
@@ -44,8 +56,9 @@ contract Graphs {
         keywords = new string[](keywordsSize);
         questions = new uint[](totalDays);
         answers = new uint[](totalDays);
+        noKeywords = new uint[](totalDays);
 
-        // add (all keywords)/(questions per day)/(answers per day) to return arrays
+        // add (all keywords)/(questions per day)/(answers per day)/(noKeywords per day) to return arrays
         uint keywordsIndex = 0;
         uint dayIndex = 0;
         for (uint tempDate = dateFrom; tempDate <= dateTo; tempDate += 86400) {
@@ -53,9 +66,9 @@ contract Graphs {
                 keywords[keywordsIndex++] = daysStorage[tempDate].keywords[k];
             }
             questions[dayIndex] = daysStorage[tempDate].questions;
-            answers[dayIndex++] = daysStorage[tempDate].answers;
+            answers[dayIndex] = daysStorage[tempDate].answers;
+            noKeywords[dayIndex++] = daysStorage[tempDate].noKeywords;
         }
-        return (keywords, questions, answers);
+        return (keywords, questions, answers, noKeywords);
     }
-
 }
